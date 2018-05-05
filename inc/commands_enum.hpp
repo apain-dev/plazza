9390be3708ee1b229f35c2d2a6fb42a8c9900c51
jibs <jean-baptiste.detroyes@epitech.eu>
2018-05-05 17:19:21 +0200

    enum

---
+++ inc/commands_enum.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2018
+** plazza
+** File description:
+**  commands_enum.hpp - jibs (CLion)
+*/
+
+#ifndef PLAZZA_COMMANDS_ENUM_HPP
+# define PLAZZA_COMMANDS_ENUM_HPP
+
+enum Information {
+	PHONE_NUMBER,
+	EMAIL_ADDRESS,
+	IP_ADDRESS
+};
+
+#endif /* PLAZZA_COMMANDS_ENUM_HPP */
